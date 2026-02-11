5850e2b6af6941352be435c3d882acb5b2560d31
meier108 <meiermichael207@gmail.com>
2026-02-11 14:12:49 +0100

    RF Learning first results

--- models/lstm_goaldirected.py
+++ models/lstm_goaldirected.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LSTMGoalDirected(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, goal_score = 1):
+    def __init__(self, vocab_size, seq_len, embedding_dim, hidden_dim, goal_score = 1):
         super().__init__()
+        self.seq_len = seq_len
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
 
         # LSTM input = Embedding + Goal Score
@@ -12,19 +13,23 @@ class LSTMGoalDirected(nn.Module):
         self.fc = nn.Linear(hidden_dim, vocab_size)
 
     def forward(self, seq, goal_score, hidden  = None):
+        batch_size, current_len = seq.shape
         embeddings = self.embedding(seq) # (batch_size, seq_len, embedding_dim)
-        goal_input = goal_score.unsqueeze(1).repeat(1, seq.size(1), 1)
+        goal_input = goal_score.unsqueeze(1).expand(-1, current_len, -1)
         complete_input = torch.cat([embeddings, goal_input], dim = -1)
 
         output, hidden = self.lstm(complete_input, hidden)
         logits = self.fc(output)
         return logits, hidden
     
+    def save_model(self, path):
+        torch.save(self.state_dict(), path)
+    
     @torch.no_grad()
     def generate(self, goal_score, device = 'cpu', temp = 1.0):
         self.eval()
         input_seq = torch.zeros((1, 1), dtype = torch.long).to(device)
-        goal_tensor = torch.tensor([goal_score], dtype=torch.float).to(device)
+        goal_tensor = torch.tensor([goal_score], dtype=torch.float).view(-1, 1).to(device)
         
         generated = []
         hidden = None
